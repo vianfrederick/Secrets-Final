cc0e782279f67cb90a770b9034150db405ee2e76
Vian <you@example.com>
2020-08-10 16:56:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,191 @@
+//jshint esversion:6
+require("dotenv").config();
+const express= require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const flash = require("connect-flash");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+app.use(session({
+  secret : "Our little secret",
+  resave : false,
+  saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+
+
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+mongoose.connect("mongodb://localhost:27017/secretsDB",{useNewUrlParser : true, useUnifiedTopology : true});
+mongoose.set("useCreateIndex", true);
+const userSchema = new mongoose.Schema({
+  email : String,
+  password : String,
+  googleId : String,
+  secret  : [String]
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL : "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+  app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets",{userSecrets : req.user.secret});
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/submit",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+
+app.post("/register",function(req,res){
+
+User.register({username : req.body.username}, req.body.password, function(err,user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  }
+  else{
+    passport.authenticate("local")(req,res, function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+
+
+});
+
+app.post("/login",
+    // const user = new User({
+    //   username : req.body.username,
+    //   password : req.body.password
+    // });
+
+    passport.authenticate("local",{
+      successRedirect : "/secrets",
+      failureRedirect : "/login",
+      failureFlash : true
+    })
+  );
+
+app.post("/submit",function(req,res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err,foundUser){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        foundUser.secret.push(submittedSecret);
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+});
